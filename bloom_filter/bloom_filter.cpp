5fd865ee4faf46ba7b3554a80f890c528821394e
Sahith Kurapati <sahith02@gmail.com>
2021-04-05 12:41:36 +0530

    added simpler version of stl bitset with set and constructor methods

---
+++ bloom_filter/bloom_filter.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+#define WORD 32
+
+class bit_vector{
+	private:
+	int _bit_size;
+	int _array_size;
+	uint32_t* _bit_array;
+
+	public:
+	bit_vector(int size) : _bit_size(size) {
+		_array_size = _bit_size / WORD + 1;
+		_bit_array = new uint32_t[_array_size];
+		for(int i = 0; i < _array_size; ++i){
+			_bit_array[i] = 0;
+		}
+	}
+
+	int array_index(int bit_pos){
+		return bit_pos / WORD;
+	}
+
+	int array_offset(int bit_pos){
+		return bit_pos % WORD;
+	}
+
+	void set(int bit_pos, bool bit_val){
+		int bit_index = array_index(bit_pos);
+		int bit_offset = array_offset(bit_pos);
+		if(bit_val){
+			_bit_array[bit_index] |= (1 << (WORD - bit_offset - 1));
+		}
+		else{
+			_bit_array[bit_index] &= ~(1 << (WORD - bit_offset - 1));
+		}
+	}
+
+	void show(){
+		uint32_t compare = (1 << 31);
+		// printing all but last element of bit array
+		int i = 0;
+		for(i = 0; i < _array_size - 1; ++i){
+			uint32_t temp = _bit_array[i];
+			for(int j = 0; j < 32; ++j){
+				cout << ((temp & compare) ? 1 : 0) << " ";
+				temp = temp << 1;
+			}
+			cout << endl;
+		}
+		// printing last element of the bit array
+		// ++i;
+		uint32_t temp = _bit_array[i];
+		int max_offset = _bit_size % WORD;
+		for(int j = 0; j < max_offset; ++j){
+			cout << ((temp & compare) ? 1 : 0) << " ";
+			temp = temp << 1;
+		}
+		cout << endl;
+	}
+};
+
+int main(){
+	bit_vector b(30);
+	b.set(2, 1);
+	// b.set(2, 0);
+	// b.set(2, 1);
+	b.show();
+	cout << "Bloom filters coming up!" << endl;
+}
