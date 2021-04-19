d16abaa1a43b53069f3a2b34ef3ea13f9ca6ea38
Sahith Kurapati <sahith02@gmail.com>
2021-04-19 19:02:58 +0530

    added strings ubuntu format

--- bloom_filter/tests/string/generate_string.py
+++ bloom_filter/tests/string/generate_string.py
@@ -1,34 +1,37 @@
 from key_generator.key_generator import generate
 
-OUTPUT_PATH = "./string_test_100.txt"
-STRING_COUNT = 100
+all_sizes_required = [(100, '100'), (500, '500'), (1000, '1k'), (5000, '5k'), (10000, '10k'), (25000, '25k'), (50000, '50k'), (100000, '100k')]
 
+for file_size in all_sizes_required:
+	OUTPUT_PATH = "./string_test_" + file_size[1] + ".txt"
+	STRING_COUNT = file_size[0]
 
-output_file = open(OUTPUT_PATH, "w")
+	output_file = open(OUTPUT_PATH, "w")
 
-for i in range(STRING_COUNT):
-	string = ""
+	for i in range(STRING_COUNT):
+		string = ""
 
-	recipient = generate(
-		num_of_atom = 1,
-		type_of_value = "hex",
-		capital = "mix",
-		extras = ["-", "_"],
-		seed = i
-	).get_key()
+		recipient = generate(
+			num_of_atom = 1,
+			type_of_value = "hex",
+			capital = "mix",
+			extras = ["-", "_"],
+			seed = i
+		).get_key()
 
-	domain = generate(
-		num_of_atom = 2,
-		separator = ".",
-		min_atom_len = 3,
-		max_atom_len = 5,
-		type_of_value = "hex",
-		capital = "mix",
-		extras = ["-"],
-		seed = i
-	).get_key()
+		domain = generate(
+			num_of_atom = 2,
+			separator = ".",
+			min_atom_len = 3,
+			max_atom_len = 5,
+			type_of_value = "hex",
+			capital = "mix",
+			extras = ["-"],
+			seed = i
+		).get_key()
 
-	string = recipient + "@" + domain
-	output_file.write(string + "\n")
+		string = recipient + "@" + domain
+		output_file.write(string + "\n")
 
-output_file.close()
+	output_file.close()
+	print("Done with " + OUTPUT_PATH)
