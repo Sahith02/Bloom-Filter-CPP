13dd03661af3c18af0d4f8277d886bd9d694e3cb
Sahith Kurapati <sahith02@gmail.com>
2021-04-19 19:05:05 +0530

    added int generate for ubuntu

--- bloom_filter/tests/int/generate_int.py
+++ bloom_filter/tests/int/generate_int.py
@@ -1,24 +1,28 @@
 from key_generator.key_generator import generate
 
-OUTPUT_PATH = "./int_test_100.txt"
-STRING_COUNT = 100
+all_sizes_required = [(100, '100'), (500, '500'), (1000, '1k'), (5000, '5k'), (10000, '10k'), (25000, '25k'), (50000, '50k'), (100000, '100k')]
+
+for file_size in all_sizes_required:
+	OUTPUT_PATH = "./int_test_" + file_size[1] + ".txt"
+	STRING_COUNT = file_size[0]
 
 
-output_file = open(OUTPUT_PATH, "w")
+	output_file = open(OUTPUT_PATH, "w")
 
-for i in range(STRING_COUNT):
-	integer = generate(
-		num_of_atom = 1,
-		min_atom_len = 3,
-		max_atom_len = 9,
-		type_of_value = "int",
-		seed = i
-	).get_key()
+	for i in range(STRING_COUNT):
+		integer = generate(
+			num_of_atom = 1,
+			min_atom_len = 3,
+			max_atom_len = 9,
+			type_of_value = "int",
+			seed = i
+		).get_key()
 
-	integer = integer.lstrip("0")
-	if(integer == ""):
-		output_file.write("0" + "\n")
-	else:
-		output_file.write(integer + "\n")
+		integer = integer.lstrip("0")
+		if(integer == ""):
+			output_file.write("0" + "\n")
+		else:
+			output_file.write(integer + "\n")
 
-output_file.close()
+	output_file.close()
+	print("Done with " + OUTPUT_PATH)
